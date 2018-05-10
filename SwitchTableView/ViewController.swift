b35653291c90254b9201ec92330772740cb35394
tham huy <thamgiahuy@admins-imac.local>
2018-05-10 08:27:30 +0700

    Initial Commit

---
+++ SwitchTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwitchTableView
+//
+//  Created by tham gia huy on 5/10/18.
+//  Copyright © 2018 tham gia huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
